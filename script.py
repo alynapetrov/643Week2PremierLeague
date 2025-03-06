5e9a747fe5ca907ed82d87f90608c2f64a8e2e63
alynapetrov <alynap@umich.edu>
2025-03-06 19:37:29 +0000

    Script fully passes pylint

--- script.py
+++ script.py
@@ -1,57 +1,64 @@
-import pandas as pd
-import numpy as np
-import altair as alt
+"""
+    Script for taking raw Premier League season data and generating a heatmap
+    displaying the difference between expected and actual away goals for each team.
+"""
+
 import argparse
 from typing import List
+import pandas as pd
+import altair as alt
 
 
 def load_data(files: List[str]) -> pd.DataFrame:
     """
     Loading data from csv files into a pandas df.
     """
-    colsToKeep = ['h.title', 'a.title', 'goals.h', 'goals.a', 'xG.h', 'xG.a']
-    xGData = pd.DataFrame()
+    cols_to_keep = ['h.title', 'a.title', 'goals.h', 'goals.a', 'xG.h', 'xG.a']
+    x_data = pd.DataFrame()
+    # Going through each file from the input list
     for i in files:
-        #data = pd.read_csv(f'PL_data/EPL_{i}_matches.csv')
         data = pd.read_csv(i)
-        data = data[colsToKeep]
+        data = data[cols_to_keep]
         # Adding a col to keep track of season
         season = int(i.split('/')[-1].split('_')[1])
         data['season'] = season
-        xGData = pd.concat([xGData, data], ignore_index=True)
-    return xGData
+        x_data = pd.concat([x_data, data], ignore_index=True)
+    return x_data
 
-def clean_data(xGData: pd.DataFrame) -> pd.DataFrame:
+def clean_data(x_g_data: pd.DataFrame) -> pd.DataFrame:
     """
     Cleaning the data
     """
-    # Only keeping teams that were in Premier League all 4 seasons (2016-2019)
-    xGoals = xGData.groupby('a.title')
-    # This filter function give us only teams that have 4 unique seasons (2016-2019)
-    xGoals = xGoals.filter(lambda x: x['season'].nunique() == 4)
+    x_goal = x_g_data.groupby('a.title')
+    # This filter function gives us only teams that were in each season (every input file)
+    x_goal = x_goal.filter(lambda x: x['season'].nunique() == x_g_data['season'].nunique())
     # Adding a data column to represent the difference between away xG and actual away goals.
-    xGoals['awayGoalDif'] = xGoals['goals.a'] - xGoals['xG.a']
-    xGoals.rename(columns={'a.title': 'team'}, inplace=True)
-    # Aggregating our data to get mean of difference between away xG and actual away goals for each season
-    xGoals = xGoals.groupby(['season','team']).agg(avgDif=('awayGoalDif','mean')).reset_index()
-    return xGoals
+    x_goal['awayGoalDif'] = x_goal['goals.a'] - x_goal['xG.a']
+    x_goal.rename(columns={'a.title': 'team'}, inplace=True)
+    # Aggregating our data to get mean of difference between away xG and actual away goals
+    x_goal = x_goal.groupby(['season','team']).agg(avgDif=('awayGoalDif','mean')).reset_index()
+    return x_goal
 
-def make_heatmap(xGoals: pd.DataFrame) -> alt.Chart:
+def make_heatmap(x_goal: pd.DataFrame) -> alt.Chart:
     """
     Creating a heatmap
     """
-    vis = alt.Chart(xGoals).mark_rect().encode(
+    vis = alt.Chart(x_goal).mark_rect().encode(
         x=alt.X("team:N",title='Team', axis=alt.Axis(labelAngle=0)),
         y=alt.Y('season:N', title='Year'),
         # Scale is the color scale we want to use, legend allows us to title the scale
-        color=alt.Color('avgDif:Q', scale=alt.Scale(scheme="redyellowgreen"),legend=alt.Legend(title="Avg Goal Difference"))
+        color=alt.Color('avgDif:Q',
+                        scale=alt.Scale(scheme="redyellowgreen"),
+                        legend=alt.Legend(title="Avg Goal Difference")
+                       )
     ).properties(
         # Setting title and size
-        title='Average Difference Between Expected Away Goals and Actual Away Goals',width=1150, height=300
+        title='Average Difference Between Expected Away Goals and Actual Away Goals',
+        width=1150, height=300
     )
     # Bold text overlay to show the difference value within the squares
     text = vis.mark_text(fontWeight='bold').encode(
-        x=alt.X("team:N",title='Team', axis=alt.Axis(labelAngle=0)), 
+        x=alt.X("team:N",title='Team', axis=alt.Axis(labelAngle=0)),
         y=alt.Y('season:N', title='Year'),
         text=alt.Text('avgDif:Q', format='.2f'),
         color=alt.value('black'),
@@ -64,9 +71,9 @@ if __name__ == '__main__':
     parser.add_argument("output", help="Output file")
     args = parser.parse_args()
     # Loading the data
-    data = load_data(args.raw_data)
+    rawdata = load_data(args.raw_data)
     # Cleaning the data
-    xGoals = clean_data(data)
+    x_goals = clean_data(rawdata)
     # Making the heatmap and saving it to the output file
-    heatmap = make_heatmap(xGoals)
+    heatmap = make_heatmap(x_goals)
     heatmap.save(args.output)
